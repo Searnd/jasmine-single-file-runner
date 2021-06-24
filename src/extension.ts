26ad55ffeedb91e5441677ea02d1dace5041dd08
Andres Flores <andres.flores@polymtl.ca>
2021-06-24 15:35:21 -0400

    first hello world from context menu

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,30 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+	function registerCommand(command: string, callback: () => void): void {
+		const disposable = vscode.commands.registerCommand(command, callback);
+
+		context.subscriptions.push(disposable);
+	}
+
+	function registerTextEditorCommand(command: string, callback: () => void): void {
+		const disposable = vscode.commands.registerTextEditorCommand(command, callback);
+
+		context.subscriptions.push(disposable);
+	}
 	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "jasmine-single-file-runner" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('jasmine-single-file-runner.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	registerCommand('jsfr.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from Jasmine Single File Runner!');
 	});
 
-	context.subscriptions.push(disposable);
+	registerTextEditorCommand('jsfr.testCurrentFile', () => {
+		vscode.window.showInformationMessage(':DDDDDDD');
+	});
 }
 
 // this method is called when your extension is deactivated
