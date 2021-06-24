40173f7ed233e0666795429af17ad86e47c60c25
Andres Flores <andres.flores@polymtl.ca>
2021-06-24 17:26:28 -0400

    adjust Glob pattern

--- src/TestFileFinder.ts
+++ src/TestFileFinder.ts
@@ -3,7 +3,7 @@ import { FileNotFoundError } from './exceptions/FileNotFoundError';
 
 export class TestFileFinder {
     public async getTestFileLocation(): Promise<Uri> {
-        const files = await workspace.findFiles("*/**/test.ts");
+        const files = await workspace.findFiles("**/test.ts");
         if (files.length < 1) {
             throw new FileNotFoundError("Error: no test.ts file found. Are you in an Angular project?");
         }
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { FileNotFoundError } from './exceptions/FileNotFoundError';
+import { TestFileEditor } from './TestFileEditor';
+import { TestFileFinder } from './TestFileFinder';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,17 +20,27 @@ export function activate(context: vscode.ExtensionContext) {
 
 		context.subscriptions.push(disposable);
 	}
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "jasmine-single-file-runner" is now active!');
 
 	registerCommand('jsfr.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from Jasmine Single File Runner!');
 	});
 
-	registerTextEditorCommand('jsfr.testCurrentFile', () => {
-		vscode.window.showInformationMessage(':DDDDDDD');
+	registerTextEditorCommand('jsfr.testCurrentFile', async () => {
+		vscode.window.showInformationMessage('Executing tests for current file...');
+		try {
+			const testFileFinder = new TestFileFinder();
+			const testFileUri = await testFileFinder.getTestFileLocation();
+
+			const testFileEditor = new TestFileEditor(testFileUri);
+			vscode.window.showInformationMessage("Success!");
+		}
+		catch(e) {
+			if (e instanceof FileNotFoundError) {
+				vscode.window.showErrorMessage(e.message);
+			} else {
+				throw e;
+			}
+		}
 	});
 }
 
