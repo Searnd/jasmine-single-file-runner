0d88915982a0291e79a3eae8033b7887c0f84c89
Andres Flores <andres.flores@polymtl.ca>
2021-06-24 23:05:15 -0400

    exec tests works

--- src/TestRunner.ts
+++ src/TestRunner.ts
@@ -1,12 +1,22 @@
 import * as cp from 'child_process';
 
+type ChildProcessExitCallback = (code: number | null, signal: NodeJS.Signals | null) => void;
+
 export class TestRunner {
-    public async execTests(): Promise<string> {
+    private _specFilePath: string;
+
+    constructor(specFilePath: string) {
+        this._specFilePath = specFilePath;
+    }
+
+    public async execTests(onExit: ChildProcessExitCallback): Promise<string> {
         return new Promise<string>((resolve, reject) => {
-            cp.exec("ng test", (err, out) => {
+            const childProcess =  cp.exec("ng test", { cwd: this._specFilePath }, (err, out) => {
                 if (err) {
+                    console.log(err);
                     reject(err);
                 }
+                childProcess.on("exit", onExit);
 
                 return resolve(out);
             });
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import * as path from 'path';
 import * as vscode from 'vscode';
 import { CommandRegistrar } from './CommandRegistrar';
 import { FileNotFoundError } from './exceptions/FileNotFoundError';
 import { LineNotFoundInFileError } from './exceptions/LineNotFoundInFileError';
 import { TestFileEditor } from './TestFileEditor';
 import { TestFileFinder } from './TestFileFinder';
+import { TestRunner } from './TestRunner';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -22,7 +24,14 @@ export function activate(context: vscode.ExtensionContext) {
 			const testFileEditor = new TestFileEditor(testFileUri);
 			testFileEditor.addSpecFileToContextLine(textEditor.document.uri);
 
-			vscode.window.showInformationMessage("Success!");
+			const specFileDirectory = path.dirname(textEditor.document.uri.fsPath);
+			const testRunner = new TestRunner(specFileDirectory);
+			testRunner.execTests(() => {
+				testFileEditor.restoreContextLine();
+			}).then(
+				() => { vscode.window.showInformationMessage("Success!"); },
+				() => { vscode.window.showErrorMessage("Error: unable to run ng test"); }
+			);
 		}
 		catch(e) {
 			if (e instanceof FileNotFoundError || e instanceof LineNotFoundInFileError) {
