84a9769aa4f3a507f358ad68232f869888845ad2
Andres Flores <andres.flores@polymtl.ca>
2021-06-25 01:40:28 -0400

    add getTask to task manager

--- src/TaskRegistrar.ts
+++ src/TaskRegistrar.ts
@@ -1,18 +1,33 @@
 import * as vscode from 'vscode';
 
-export class TaskRegistrar {
+export class TaskManager {
+    private _type: string;
 
-    public registerTaskProvider(type: string, commandLine: string, cwd?: string): vscode.Disposable {
-        return vscode.tasks.registerTaskProvider(type, {
-            provideTasks() {
+    constructor(type: string) {
+        this._type = type;
+    }
+
+    public registerTaskProvider(commandLine: string, cwd?: string): vscode.Disposable {
+        return vscode.tasks.registerTaskProvider(this._type, {
+            provideTasks: () => {
                 const execution = new vscode.ShellExecution(commandLine, {cwd});
+                const task = new vscode.Task({type: this._type}, vscode.TaskScope.Workspace, "ng test", "jsfr", execution);
+
                 return [
-                    new vscode.Task({type}, vscode.TaskScope.Workspace, "Test", "jsfr", execution)
+                    task
                 ];
             },
-            resolveTask(task) {
+            resolveTask: (task) => {
                 return task;
             }
+        });
+    }
+
+    public async getTask(name: string): Promise<vscode.Task|undefined> {
+        const tasks = await vscode.tasks.fetchTasks({type: this._type});
+
+        return tasks.find((task) => {
+            return task.name === name;
         });
     }
 }
