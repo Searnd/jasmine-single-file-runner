23cc893db26d72358cb67b5eae0a97a94ed80939
Andres Flores <andres.flores@polymtl.ca>
2021-07-01 23:18:33 -0400

    add spec -> test suite info mapper

--- src/models/spec-complete-response.ts
+++ src/models/spec-complete-response.ts
@@ -2,14 +2,14 @@ import { TestResult } from "../enums/test-status.enum";
 
 export class SpecCompleteResponse {
   public constructor(
-    public _id: string,
-    public _failureMessages: string[],
-    public _suite: string[],
-    public _description: string,
-    public _fullName: string,
-    public _status: TestResult,
-    public _timeSpentInMilliseconds: string,
-    public _filePath?: string,
-    public _line?: number
+    public id: string,
+    public failureMessages: string[],
+    public suite: string[],
+    public description: string,
+    public fullName: string,
+    public status: TestResult,
+    public timeSpentInMilliseconds: string,
+    public filePath?: string,
+    public line?: number
   ) { }
 }
