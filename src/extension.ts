e4bde8d445a21fa4ed5d202ccfe4885b8029c2e4
Andres Flores <andres.flores@polymtl.ca>
2021-07-01 22:49:13 -0400

    set up adapter

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,40 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { testExplorerExtensionId, TestHub } from 'vscode-test-adapter-api';
+import { Log, TestAdapterRegistrar } from 'vscode-test-adapter-util';
 import { CommandRegistrar } from './CommandRegistrar';
 import { Coordinator } from './Coordinator';
 import { FileNotFoundError, LineNotFoundInFileError } from './exceptions/error-index';
+import { JsfrAdapter } from './jsfr-adapter';
 
 let ngTestProvider: vscode.Disposable | undefined;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+	const workspaceFolder = (vscode.workspace.workspaceFolders || [])[0];
+
+	const log = new Log("JSFR", workspaceFolder, "JSFR Log");
+	context.subscriptions.push(log);
+
+	const testExplorerExtension = vscode.extensions.getExtension<TestHub>(testExplorerExtensionId);
+	if (log.enabled) {
+		log.info(`Test Explorer ${testExplorerExtension ? '' : 'not '}found`);
+	}
+
+	if (testExplorerExtension) {
+		const testHub = testExplorerExtension.exports;
+
+		const testAdapterRegistrar = new TestAdapterRegistrar(
+			testHub,
+			workspaceFolder => new JsfrAdapter(workspaceFolder, log),
+			log
+		);
+
+		context.subscriptions.push(testAdapterRegistrar);
+	}
 
 	const commandRegistrar = new CommandRegistrar(context);
 
