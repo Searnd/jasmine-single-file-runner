4e5c4beafc73920ac99c5b524fac23a8b187b819
Andres Flores <andres.flores@polymtl.ca>
2021-07-03 17:54:30 -0400

    implement backupFile

--- src/infrastructure/file-editors/tsconfig-spec-editor.ts
+++ src/infrastructure/file-editors/tsconfig-spec-editor.ts
@@ -1,4 +1,4 @@
-import { TextDocument, Uri } from "vscode";
+import { FileSystemError, TextDocument, Uri } from "vscode";
 
 export class TsConfigSpecEditor {
     private _tsconfigInitialData: string | undefined;
@@ -7,4 +7,11 @@ export class TsConfigSpecEditor {
         private _tsconfigSpecFileUri: Uri,
         private _specFile: TextDocument
     ) {}
+
+    private backupFile(data: string): void {
+        if (!data.length) {
+                throw new FileSystemError("Error: unable to back up file");
+        }
+        this._tsconfigInitialData = data;
+    }
 }
