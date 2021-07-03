3a70bb9e73ee3b7a192e5916c25832a6a5a14392
Andres Flores <andres.flores@polymtl.ca>
2021-07-03 11:56:33 -0400

    fix lint errors

--- src/config/karma-configurator.ts
+++ src/config/karma-configurator.ts
@@ -9,7 +9,7 @@ export class KarmaConfigurator {
     this.testExplorerHelper = new UtilityHelper();
   }
 
-  public setMandatoryOptions(config: Config) {
+  public setMandatoryOptions(config: Config): void {
     // remove 'logLevel' changing
     // https://github.com/karma-runner/karma/issues/614 is ready
     config.logLevel = config.LOG_INFO;
@@ -27,13 +27,13 @@ export class KarmaConfigurator {
     };
   }
 
-  public dontLoadOriginalConfigurationFileIntoBrowser(config: Config, originalConfigPath: string) {
+  public dontLoadOriginalConfigurationFileIntoBrowser(config: Config, originalConfigPath: string): void {
     // https://github.com/karma-runner/karma-intellij/issues/9
     config.exclude = config.exclude || [];
     config.exclude.push(originalConfigPath);
   }
 
-  public setBasePath(config: Config, originalConfigPath: string) {
+  public setBasePath(config: Config, originalConfigPath: string): void {
     if (!config.basePath) {
       // We need to set the base path, so karma won't use this file to base everything of
       if (originalConfigPath) {
@@ -44,7 +44,7 @@ export class KarmaConfigurator {
     }
   }
 
-  public disableSingleRunPermanently(config: Config) {
+  public disableSingleRunPermanently(config: Config): void {
     const prevSet = config.set;
     if (typeof prevSet === "function") {
       config.set = (newConfig: ConfigOptions) => {
@@ -58,12 +58,12 @@ export class KarmaConfigurator {
     }
   }
 
-  public cleanUpReporters(config: Config) {
+  public cleanUpReporters(config: Config): void {
     const filteredReporters = this.testExplorerHelper.removeElementsFromArrayWithoutModifyingIt(config.reporters, ["dots", "kjhtml"]);
     config.reporters = filteredReporters;
   }
 
-  public loadOriginalUserConfiguration(config: Config, originalConfigPath: string) {
+  public loadOriginalUserConfiguration(config: Config, originalConfigPath: string): void {
     let originalConfigModule = require(originalConfigPath);
     // https://github.com/karma-runner/karma/blob/v1.7.0/lib/config.js#L364
     if (typeof originalConfigModule === "object" && typeof originalConfigModule.default !== "undefined") {
@@ -73,7 +73,7 @@ export class KarmaConfigurator {
     originalConfigModule(config);
   }
 
-  public configureTestExplorerCustomReporter(config: Config) {
+  public configureTestExplorerCustomReporter(config: Config): void {
     this.addPlugin(config, { [`reporter:${TestExplorerCustomReporter.name}`]: ["type", TestExplorerCustomReporter.instance] });
     if (!config.reporters) {
       config.reporters = [];
@@ -81,7 +81,7 @@ export class KarmaConfigurator {
     config.reporters.push(TestExplorerCustomReporter.name);
   }
 
-  private addPlugin(karmaConfig: ConfigOptions, karmaPlugin: any) {
+  private addPlugin(karmaConfig: ConfigOptions, karmaPlugin: any): void {
     karmaConfig.plugins = karmaConfig.plugins || ["karma-*"];
     karmaConfig.plugins.push(karmaPlugin);
   }
