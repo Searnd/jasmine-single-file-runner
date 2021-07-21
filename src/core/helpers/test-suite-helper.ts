03f638350b81a86f5562e4c1bd37e8f949bf6c1e
Andres Flores <andres.flores@polymtl.ca>
2021-07-20 20:48:49 -0400

    add test suite helper

---
+++ src/core/helpers/test-suite-helper.ts
@@ -0,0 +1,33 @@
+import { KarmaTestInfo, KarmaTestSuiteInfo } from "../../domain/models/karma-test-suite-info";
+
+export class TestSuiteHelper {
+    public static findNode(searchNode: KarmaTestSuiteInfo | KarmaTestInfo | undefined, id: string): KarmaTestSuiteInfo | KarmaTestInfo | undefined {
+        if (searchNode?.id === id) {
+            return searchNode;
+        } else if (searchNode?.type === "suite") {
+            for (const child of searchNode.children) {
+                const found = this.findNode(child, id);
+                if (found){
+                    return found;
+                }
+            }
+        }
+
+        return undefined;
+    }
+
+    public static findNodeByFullName(searchNode: KarmaTestSuiteInfo | KarmaTestInfo | undefined, fullName: string): KarmaTestSuiteInfo | KarmaTestInfo | undefined {
+        if (searchNode?.fullName === fullName) {
+            return searchNode;
+        } else if (searchNode?.type === "suite") {
+            for (const child of searchNode.children) {
+                const found = this.findNode(child, fullName);
+                if (found){
+                    return found;
+                }
+            }
+        }
+
+        return undefined;
+    }
+}
