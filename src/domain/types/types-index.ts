9df0f967944847a67d0a674bea23e7d8d0d131b7
Andres Flores <andres.flores@outlook.fr>
2023-03-16 21:17:52 -0400

    remove test explorer code

--- src/domain/types/types-index.ts
+++ src/domain/types/types-index.ts
@@ -1,11 +1,4 @@
 import { Uri } from "vscode";
-import { TestEvent, TestLoadFinishedEvent, TestLoadStartedEvent, TestRunFinishedEvent, TestRunStartedEvent, TestSuiteEvent } from "vscode-test-adapter-api";
-
-export type TestLoadEvent = TestLoadStartedEvent | TestLoadFinishedEvent;
-
-export type TestRunEvent = TestRunStartedEvent | TestRunFinishedEvent;
-
-export type TestStateEvent = TestRunEvent | TestSuiteEvent | TestEvent;
 
 export type KarmaConfig = {
     port: number,
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { testExplorerExtensionId, TestHub } from "vscode-test-adapter-api";
-import { Log, TestAdapterRegistrar } from "vscode-test-adapter-util";
 import { CommandRegistrar } from "./core/command-registrar";
 import { Coordinator } from "./core/coordinator";
 import { Logger, OUTPUT_CHANNEL } from "./core/helpers/logger";
-import { JsfrAdapter } from "./core/jsfr-adapter";
 import { IUri } from "./domain/types/types-index";
 
 let coordinator: Coordinator | undefined;
@@ -18,29 +15,6 @@ export const GLOBAL_LOGGER: Logger = new Logger(channel);
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext): void {
-
-	const workspaceFolder = (vscode.workspace.workspaceFolders || [])[0];
-
-	const log = new Log("JSFR", workspaceFolder, "JSFR Log");
-	context.subscriptions.push(log);
-
-	const testExplorerExtension = vscode.extensions.getExtension<TestHub>(testExplorerExtensionId);
-	if (log.enabled) {
-		log.info(`Test Explorer ${testExplorerExtension ? "" : "not "}found`);
-	}
-
-	if (testExplorerExtension) {
-		// const testHub = testExplorerExtension.exports;
-
-		// const testAdapterRegistrar = new TestAdapterRegistrar(
-		// 	testHub,
-		// 	workspaceFolder => new JsfrAdapter(workspaceFolder, log),
-		// 	log
-		// );
-
-		// context.subscriptions.push(testAdapterRegistrar);
-	}
-
 	const commandRegistrar = new CommandRegistrar(context);
 
 	commandRegistrar.registerPaletteCommand("jsfr.testCurrentFile", (vscodeResourceUri: vscode.Uri) => {
@@ -57,14 +31,19 @@ export function activate(context: vscode.ExtensionContext): void {
 			title: "JSFR",
 			location: vscode.ProgressLocation.Notification
 		};
+
 		vscode.window.withProgress(progressOptions, async (progress) => {
 			progress.report({message: "Preparing..."});
 			try {
 				coordinator = new Coordinator(resourceUri);
+				
 				await coordinator.executeTests();
 			}
 			catch(e) {
-				vscode.window.showErrorMessage(e.message);
+				if (e instanceof Error) {
+					vscode.window.showErrorMessage(e.message);
+				}
+
 				throw e;
 			}
 		});
